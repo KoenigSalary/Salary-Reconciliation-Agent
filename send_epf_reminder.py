c0ca4c36a6f87216c735214d518febf6ad6dd2c7
KoenigSalary <praveen.chaudhary@koenig-solutions.com>
2025-06-12 14:19:34 +0530

    Add files via upload

--- send_epf_reminder.py
+++ send_epf_reminder.py
@@ -1,17 +1,17 @@
-from salary_automation import setup_environment, send_epf_reminder
-import smtplib
-from email.mime.text import MIMEText
-
 def send_epf_reminder(smtp_user, smtp_pass):
+    import smtplib
+    from email.mime.text import MIMEText
+
     to_email = "tax@koenig-solutions.com"
     subject = "EPF Upload Reminder"
     body = """Dear Team,
 
-This is a friendly reminder to upload the EPF sheet for this month.
+This is a friendly reminder to upload the EPF sheet for last month.
 
-Regards,
+Regards,  
 Tax Team
 """
+
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = smtp_user
@@ -21,11 +21,3 @@ Tax Team
         server.starttls()
         server.login(smtp_user, smtp_pass)
         server.send_message(msg)
-
-if __name__ == "__main__":
-    try:
-        folder, smtp_user, smtp_pass = setup_environment()
-        send_epf_reminder(smtp_user, smtp_pass)
-        print("EPF Reminder Email Sent Successfully!")
-    except Exception as e:
-        print(f"Error sending EPF Reminder: {e}")
